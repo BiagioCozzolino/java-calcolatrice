07f2286d9ada1d6ec14510a556386f25fd701113
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-06-23 16:19:21 +0200

    Testati i metodi static in CalcoliTest completata cosegna base

--- src/jana60/CalcoliTest.java
+++ src/jana60/CalcoliTest.java
@@ -5,6 +5,12 @@ public class CalcoliTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println(CalcoliHelper.somma(6, 8));
+		System.out.println(CalcoliHelper.differenza(9, 18));
+		System.out.println(CalcoliHelper.moltiplicazione(14, 3));
+		System.out.println(CalcoliHelper.assoluto(-6));
+		System.out.println(CalcoliHelper.minore(18, 5));
+		System.out.println(CalcoliHelper.maggiore(15, 2));
 	}
 
 }
